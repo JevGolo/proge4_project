6efe636313eae74f849b81394e42643802fa7f88
JevGolo <jev.golosov@gmail.com>
2023-10-08 21:44:24 +0300

    Created all pages designs, styles. Updated app.py

--- src/app.py
+++ src/app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request
 
-app = Flask (__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return "<h2>Hello world</h2>"
+    return redirect("/main")
+
+
+@app.route("/main")
+def main():
+    return render_template("main.html")
+
+
+@app.route("/calc")
+def calc():
+    return render_template("calc.html")
+
+
+@app.route("/result", methods=['POST', 'GET'])
+def result():
+    if request.method == "POST":
+        reaction = request.form['reaction']
+        if reaction.strip() == "":
+            return redirect("/calc")
+        else:
+            return render_template("result.html")
+    else:
+        return redirect("/calc")
+
+
+@app.route("/help")
+def help():
+    return render_template("help.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run(host = "192.168.0.105", port = 80, debug=True)
